9b4496c136e7ade46519f9d7bf4681289258624b
Zack Osowski <62389933+zackosowski@users.noreply.github.com>
2022-09-19 08:08:00 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,9 +5,9 @@
 import main;
 import datetime;
 
-year = 2021
-month = 12
-day = 30
+year = 2022
+month = 9
+day = 22
 
 def test_code():
     assert main.product3Plus1(2,2,2) == 9, "product3Plus1(2, 2, 2) == 9 failed"
